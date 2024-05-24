fba9c0c5a57b1b104dfe1f0fb7d9bc306b0f9e6f
unknown <nihalsoni36@gmail.com>
2024-05-24 11:53:26 +0530

    Revert "Test3"
    This reverts commit 7caa639035b0a041a54b19dfa898bec179c69cf4, reversing
    changes made to 57bf3344cdde162e1c55eeedf70d18b2dce9e971.

--- Calculator-App-in-Spring-Boot-master/src/main/java/com/example/demo/CalculatorSampleAppApplication.java
+++ Calculator-App-in-Spring-Boot-master/src/main/java/com/example/demo/CalculatorSampleAppApplication.java
@@ -8,12 +8,8 @@ public class CalculatorSampleAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CalculatorSampleAppApplication.class, args);
-<<<<<<< HEAD
 		//Test changes I love Neha I love Neha Till we live
 		
-=======
-		//Test changes I love Neha ilove only Neha
->>>>>>> master
 		//changed again
 	}
 }
